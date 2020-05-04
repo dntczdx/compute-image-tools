cbfdce9e56f7ea53248d8d8a165ee4a4b47bc337
Tony Zhang <tzz@google.com>
2020-05-04 16:57:45 -0700

    e2e test for wrapper

---
+++ cli_tools_e2e_test/gce_windows_upgrade/test_suites/windows_upgrade/test_suite.go
@@ -0,0 +1,132 @@
+//  Copyright 2020 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+// Package windowsupgrade contains e2e tests for gce_windows_upgrade
+package windowsupgrade
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"regexp"
+	"sync"
+
+	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/path"
+	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/gce_windows_upgrade/upgrader"
+	"github.com/GoogleCloudPlatform/compute-image-tools/gce_image_import_export_tests/compute"
+	clitoolstestutils "github.com/GoogleCloudPlatform/compute-image-tools/go/e2e_test_utils/cli_tools"
+	"github.com/GoogleCloudPlatform/compute-image-tools/go/e2e_test_utils/junitxml"
+	"github.com/GoogleCloudPlatform/compute-image-tools/go/e2e_test_utils/test_config"
+)
+
+const (
+	testSuiteName = "WindowsUpgradeTests"
+)
+
+// TestSuite contains implementations of the e2e tests.
+func TestSuite(
+	ctx context.Context, tswg *sync.WaitGroup, testSuites chan *junitxml.TestSuite,
+	logger *log.Logger, testSuiteRegex, testCaseRegex *regexp.Regexp,
+	testProjectConfig *testconfig.Project) {
+
+	testTypes := []clitoolstestutils.CLITestType{
+		clitoolstestutils.Wrapper,
+	}
+
+	testsMap := map[clitoolstestutils.CLITestType]map[*junitxml.TestCase]func(
+		context.Context, *junitxml.TestCase, *log.Logger, *testconfig.Project, clitoolstestutils.CLITestType){}
+
+	for _, testType := range testTypes {
+		normalCase := junitxml.NewTestCase(
+			testSuiteName, fmt.Sprintf("[%v] %v", testType, "Normal case"))
+		richParamsAndLatestInstallMedia := junitxml.NewTestCase(
+			testSuiteName, fmt.Sprintf("[%v] %v", testType, "Rich params and latest install media"))
+
+		testsMap[testType] = map[*junitxml.TestCase]func(
+			context.Context, *junitxml.TestCase, *log.Logger, *testconfig.Project, clitoolstestutils.CLITestType){}
+		testsMap[testType][normalCase] = runWindowsUpgradeNormalCase
+		testsMap[testType][richParamsAndLatestInstallMedia] = runWindowsUpgradeWithRichParamsAndLatestInstallMedia
+	}
+	clitoolstestutils.CLITestSuite(ctx, tswg, testSuites, logger, testSuiteRegex, testCaseRegex,
+		testProjectConfig, testSuiteName, testsMap)
+}
+
+func runWindowsUpgradeNormalCase(ctx context.Context, testCase *junitxml.TestCase, logger *log.Logger,
+	testProjectConfig *testconfig.Project, testType clitoolstestutils.CLITestType) {
+
+	suffix := path.RandString(5)
+
+	argsMap := map[clitoolstestutils.CLITestType][]string{
+		clitoolstestutils.Wrapper: {
+			"-client-id=e2e",
+			fmt.Sprintf("-source-os=%v", "windows-2008r2"),
+			fmt.Sprintf("-target-os=%v", "windows-2012r2"),
+			fmt.Sprintf("-instance=projects/%v/zones/%v/instances/test-upgrade-%v",
+				testProjectConfig.TestProjectID, testProjectConfig.TestZone, suffix),
+		},
+	}
+	runTest(ctx, argsMap[testType], testType, testProjectConfig, imageName, logger, testCase)
+}
+
+func runWindowsUpgradeWithRichParamsAndLatestInstallMedia(ctx context.Context, testCase *junitxml.TestCase, logger *log.Logger,
+		testProjectConfig *testconfig.Project, testType clitoolstestutils.CLITestType) {
+
+	suffix := path.RandString(5)
+
+	// TODO: switch to the latest install media when it's ready
+	argsMap := map[clitoolstestutils.CLITestType][]string{
+		clitoolstestutils.Wrapper: {
+			"-client_id=e2e",
+			fmt.Sprintf("-source-os=%v", "windows-2008r2"),
+			fmt.Sprintf("-target-os=%v", "windows-2012r2"),
+			fmt.Sprintf("-instance=projects/%v/zones/%v/instances/test-upgrade-%v",
+				testProjectConfig.TestProjectID, testProjectConfig.TestZone, suffix),
+			fmt.Sprintf("-skip-machine-image-backup"),
+			fmt.Sprintf("-auto-rollback"),
+			fmt.Sprintf("-timeout=2h"),
+			fmt.Sprintf("-project=%v", "compute-image-test-pool-002"),
+			fmt.Sprintf("-zone=%v", "fake-zone"),
+		},
+	}
+	runTest(ctx, argsMap[testType], testType, testProjectConfig, imageName, logger, testCase)
+}
+
+func runTest(ctx context.Context, args []string, testType clitoolstestutils.CLITestType,
+	testProjectConfig *testconfig.Project, imageName string, logger *log.Logger, testCase *junitxml.TestCase) {
+
+	// gcloud is not ready yet. However, it's harmless to keep the command name here.
+	cmds := map[clitoolstestutils.CLITestType]string{
+		clitoolstestutils.Wrapper:                   "./gce_windows_upgrade",
+		clitoolstestutils.GcloudProdWrapperLatest:   "gcloud",
+		clitoolstestutils.GcloudLatestWrapperLatest: "gcloud",
+	}
+
+	if clitoolstestutils.RunTestForTestType(cmds[testType], args, testType, logger, testCase) {
+		verifyUpgradedInstance(ctx, testCase, testProjectConfig, imageName, logger)
+	}
+}
+
+func verifyUpgradedInstance(ctx context.Context, testCase *junitxml.TestCase,
+	testProjectConfig *testconfig.Project, imageName string, logger *log.Logger) {
+
+	// get instance object
+
+	// verify startup script & backup
+	// verify failure reason
+
+	// set startup script
+	// restart, verify OS version
+
+	// delete instance
+}
--- cli_tools_e2e_test/go.mod
+++ cli_tools_e2e_test/go.mod
@@ -1,17 +1,15 @@
-module github.com/GoogleCloudPlatform/compute-image-tools/cli_tools_e2e_test
+module github.com/GoogleCloudPlatform/compute-image-tools/gce_image_import_export_tests
 
 go 1.13
 
 require (
-	cloud.google.com/go v0.57.0 // indirect
-	cloud.google.com/go/storage v1.7.0
-	github.com/GoogleCloudPlatform/compute-image-tools/cli_tools v0.0.0-20200508225712-3c09ffd2f054
-	github.com/GoogleCloudPlatform/compute-image-tools/daisy v0.0.0-20200508225712-3c09ffd2f054
-	github.com/GoogleCloudPlatform/compute-image-tools/go/e2e_test_utils v0.0.0-20200508225712-3c09ffd2f054
-	github.com/golang/protobuf v1.4.1 // indirect
-	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
-	golang.org/x/sys v0.0.0-20200508214444-3aab700007d7 // indirect
-	golang.org/x/tools v0.0.0-20200508232336-ead0a569305d // indirect
-	google.golang.org/api v0.23.0
-	google.golang.org/genproto v0.0.0-20200507105951-43844f6eee31 // indirect
+	cloud.google.com/go/storage v1.6.0
+	github.com/GoogleCloudPlatform/compute-image-tools/cli_tools v0.0.0-20200504155228-251922788421
+	github.com/GoogleCloudPlatform/compute-image-tools/daisy v0.0.0-20200504155228-251922788421
+	github.com/GoogleCloudPlatform/compute-image-tools/go/e2e_test_utils v0.0.0-20200504155228-251922788421
+	golang.org/x/net v0.0.0-20200501053045-e0ff5e5a1de5 // indirect
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+	golang.org/x/tools v0.0.0-20200504152539-33427f1b0364 // indirect
+	google.golang.org/api v0.22.0
+	google.golang.org/genproto v0.0.0-20200430143042-b979b6f78d84 // indirect
 )
